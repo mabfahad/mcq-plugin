f6c99fd25e9777aa9f1467d01dd7728174a02949
Fahad <fahad.abdullah1280@gmail.com>
2022-04-09 13:59:36 +0600

    showing all quizzes, questions and answers are done

--- classes/Quiz.php
+++ classes/Quiz.php
@@ -19,8 +19,71 @@ class Quiz
                     <div class="quiz-title"><?php
                         echo $quiz->post_title; ?></div>
                     <div class="quiz-content"><?php
-                        echo $quiz->post_content; ?></div>
+                        //echo $quiz->post_content; ?></div>
                     <div class="quiz-questions">
+                        <?php
+                        $args = array(
+                            'post_type'      => 'questions',
+                            'post_status'    => 'publish',
+                            'posts_per_page' => -1,
+                            'orderby'        => 'date',
+                            'order'          => 'DESC',
+                            'meta_query'     => array(
+                                array(
+                                    'key'     => '_quiz_id',
+                                    'value'   => $quiz->ID,
+                                    'compare' => '=',
+                                ),
+                            ),
+                        );
+                        $questions = get_posts($args);
+                        ?>
+                        <ul>
+                            <?php
+                                foreach ($questions as $question) {
+                                    ?>
+                                    <li>
+                                        <div class="question-title"><?php
+                                            echo $question->post_title; ?></div>
+                                        <div class="question-content"><?php
+                                            echo $question->post_content; ?></div>
+                                        <div class="question-answers">
+                                            <?php
+                                            $args = array(
+                                                'post_type'      => 'answers',
+                                                'post_status'    => 'publish',
+                                                'posts_per_page' => -1,
+                                                'orderby'        => 'date',
+                                                'order'          => 'DESC',
+                                                'meta_query'     => array(
+                                                    array(
+                                                        'key'     => '_question_id',
+                                                        'value'   => $question->ID,
+                                                        'compare' => '=',
+                                                    ),
+                                                ),
+                                            );
+                                            $answers = get_posts($args);
+                                            ?>
+                                            <ul>
+                                                <?php
+                                                    foreach ($answers as $answer) {
+                                                        ?>
+                                                        <li>
+                                                            <div class="answer-title"><?php
+                                                                echo $answer->post_title; ?></div>
+                                                        </li>
+                                                        <?php
+                                                    }
+                                                ?>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                    <?php
+                                }
+                            ?>
+                            <li></li>
+                        </ul>
 
                     </div>
                 </div>
